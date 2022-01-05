7c0f5f9a9e881cd1f63a4c503037d20d1385eb9c
Catkaikai <2289751443@qq.com>
2022-01-05 22:47:16 +0800

    Initial commit

---
+++ WordGameServer/src/main/java/cool/kaikai/Application.java
@@ -0,0 +1,16 @@
+package cool.kaikai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kaikai
+ * @createTime 2022年01月05日 22:23
+ * @Description : springboot启动类
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
